b7ef8ab9bb3a6aabe1688276cd420f16f6798cb7
Domenika Delgado <Domenika Delgado@Pc_Hp>
2019-04-09 01:31:55 -0500

    Creacion del constructor, y implementacion de la interfaz Accion en la clase Ave

--- src/ec/edu/ups/animales/Ave.java
+++ src/ec/edu/ups/animales/Ave.java
@@ -5,10 +5,42 @@
  */
 package ec.edu.ups.animales;
 
+import ec.edu.ups.interfaz.Accion;
+
 /**
  *
  * @author Domenika Delgado
  */
-public class Ave {
+public class Ave extends Animal implements Accion {
+    
+     public Ave(String nombre, int edad, double peso, String sexo) {
+        super(nombre, edad, peso, sexo);
+    }
+    
+    @Override
+    public void comer() {
+        System.out.println("El ave " + this.getNombre() + " está comiendo.");
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println("El ave " + this.getNombre() + " está durmiendo.");
+    }
+
+    @Override
+    public void moverse() {
+        System.out.println("El ave " + this.getNombre() + " está moviendose.");
+    }
+
+    @Override
+    public void jugar() {
+        System.out.println("El ave " + this.getNombre() + " está jugando.");
+    }
+
+    @Override
+    public void reproducirse() {
+        System.out.println("El ave " + this.getNombre() + " está reproduciendose.");
+    }
+    
     
 }
