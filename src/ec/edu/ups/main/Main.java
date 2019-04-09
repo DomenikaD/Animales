7e7d73e9f1400307deabc8bb035f2dd23f82e554
Domenika Delgado <Domenika Delgado@Pc_Hp>
2019-04-09 01:40:48 -0500

    Creacion de clase Main

--- src/ec/edu/ups/main/Main.java
+++ src/ec/edu/ups/main/Main.java
@@ -5,6 +5,10 @@
  */
 package ec.edu.ups.main;
 
+import ec.edu.ups.animales.Ave;
+import ec.edu.ups.animales.Perro;
+import ec.edu.ups.animales.Pez;
+
 /**
  *
  * @author Domenika Delgado
@@ -15,7 +19,51 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Perro perro1 = new Perro("Cookie", 2, 20, "Macho");
+        Perro perro2 = new Perro("Diva", 2, 18, "Hembra");
+        
+        perro1.comer();
+        perro1.dormir();
+        perro1.jugar();
+        perro1.moverse();
+        perro1.reproducirse();
+        
+        perro2.comer();
+        perro2.dormir();
+        perro2.jugar();
+        perro2.moverse();
+        perro2.reproducirse();
+        
+        Ave ave1 = new Ave("Larry", 3, 2, "Macho");
+        Ave ave2 = new Ave("Lily", 3, 1.5, "Hembra");
+        
+        ave1.comer();
+        ave1.dormir();
+        ave1.jugar();
+        ave1.moverse();
+        ave1.reproducirse();
+        
+        ave2.comer();
+        ave2.dormir();
+        ave2.jugar();
+        ave2.moverse();
+        ave2.reproducirse();
+        
+        Pez pez1 = new Pez("Nemo", 1, 5.5, "Macho");
+        Pez pez2 = new Pez("Dory", 1, 4.5, "Hembra");
+        
+        pez1.comer();
+        pez1.dormir();
+        pez1.jugar();
+        pez1.moverse();
+        pez1.reproducirse();
+        
+        pez2.comer();
+        pez2.dormir();
+        pez2.jugar();
+        pez2.moverse();
+        pez2.reproducirse();
     }
     
 }
